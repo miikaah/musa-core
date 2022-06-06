6350fe56b3c605a193598387fa68860c2580e19e
Miika Henttonen <miika.henttonen@wunderdog.fi>
2022-06-06 14:44:17 +0300

    Add tests for fs-state

---
+++ fixtures/state.fixture.ts
@@ -0,0 +1,22 @@
+import { State } from "../src/fs-state.types";
+
+export const stateFixture: State = {
+  isInit: null,
+  currentTheme: {
+    id: "Q01YL0Fsa3V0ZW9zL0Fsa3V0ZW9zLmpwZw",
+    filename: "CMX/Alkuteos/Alkuteos.jpg",
+    colors: {
+      bg: [255, 255, 255],
+      primary: [255, 0, 0],
+      secondary: [0, 255, 0],
+      typography: "#000",
+      typographyGhost: "#4a4a4a",
+      typographyPrimary: "#fbfbfb",
+      typographySecondary: "#000",
+      slider: [127, 127, 127],
+    },
+  },
+  replaygainType: "album",
+  volume: 76,
+  musicLibraryPath: "/Users/foobar/Music",
+};
---
+++ src/fs-state.spec.ts
@@ -0,0 +1,70 @@
+import fs from "fs/promises";
+
+import { stateFixture } from "../fixtures/state.fixture";
+import { setState, getState } from "./fs-state";
+
+jest.mock("fs/promises");
+(fs.readFile as jest.MockedFunction<typeof fs.readFile>).mockResolvedValue(
+  JSON.stringify(stateFixture)
+);
+
+const origConsoleErrorFn = console.error;
+
+describe("File system state tests", () => {
+  beforeAll(() => {
+    console.error = () => undefined;
+  });
+
+  afterAll(() => {
+    console.error = origConsoleErrorFn;
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("setState()", () => {
+    it("should call writeFile", async () => {
+      await setState("foo.json", stateFixture);
+
+      expect(fs.writeFile).toHaveBeenCalledTimes(1);
+      expect(fs.writeFile).toHaveBeenCalledWith(expect.any(String), expect.any(String));
+    });
+  });
+
+  describe("getState()", () => {
+    it("should call readFile", async () => {
+      const state = await getState("foo.json");
+
+      expect(state).toEqual(stateFixture);
+      expect(fs.readFile).toHaveBeenCalledTimes(1);
+      expect(fs.readFile).toHaveBeenCalledWith(expect.any(String), { encoding: "utf-8" });
+    });
+
+    it("should return empty JSON object if readFile throws", async () => {
+      (fs.readFile as jest.MockedFunction<typeof fs.readFile>).mockImplementationOnce(async () => {
+        throw new Error("");
+      });
+
+      const state = await getState("foo.json");
+
+      expect(state).toEqual({});
+      expect(fs.readFile).toHaveBeenCalledTimes(1);
+      expect(fs.readFile).toHaveBeenCalledWith(expect.any(String), { encoding: "utf-8" });
+    });
+
+    it("should return empty musicLibrarypath string if JSON.parse throws", async () => {
+      const origJsonParse = JSON.parse;
+      JSON.parse = jest.fn().mockImplementationOnce(() => {
+        throw new Error("");
+      });
+
+      const state = await getState("foo.json");
+      JSON.parse = origJsonParse;
+
+      expect(state).toEqual({ musicLibraryPath: "" });
+      expect(fs.readFile).toHaveBeenCalledTimes(1);
+      expect(fs.readFile).toHaveBeenCalledWith(expect.any(String), { encoding: "utf-8" });
+    });
+  });
+});
