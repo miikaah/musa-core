975bd87d15c1085f72123e5483c4bb65a7291550
Miika Henttonen <miika.henttonen@wunderdog.fi>
2022-06-07 13:55:11 +0300

    Add test for findRandom

---
+++ src/api/find.spec.ts
@@ -0,0 +1,45 @@
+import * as Scanner from "../scanner";
+import { findRandom } from "./find";
+import { getArtistAlbums } from "./artist";
+import { getAlbumById } from "./album";
+import { getAudioById } from "./audio";
+import { artistCollectionFixture } from "../../fixtures/artist.fixture";
+import { albumCollectionFixture } from "../../fixtures/album.fixture";
+import { audioCollectionFixture, audioFixture } from "../../fixtures/audio.fixture";
+
+jest.mock("./album");
+jest.mock("./artist");
+jest.mock("./audio");
+(getAudioById as jest.MockedFunction<typeof getAudioById>).mockResolvedValue(audioFixture);
+
+const artistId = "QWxhbWFhaWxtYW4gdmFzYXJhdA";
+const albumId = "QWxhbWFhaWxtYW4gdmFzYXJhdC9WYXNhcmFhc2lh";
+const audioId =
+  "QWxhbWFhaWxtYW4gdmFzYXJhdC9WYXNhcmFhc2lhLzAxIC0gTWFtZWx1a2tpICYgTXVzdGEgTGVza2kubXAz";
+// @ts-expect-error it ain't read-only silly
+Scanner.artistsForFind = [{ ...artistCollectionFixture[artistId], id: artistId }];
+// @ts-expect-error it ain't read-only silly
+Scanner.albumsForFind = [{ ...albumCollectionFixture[albumId], id: albumId }];
+// @ts-expect-error it ain't read-only silly
+Scanner.audiosForFind = [{ ...audioCollectionFixture[audioId], id: audioId }];
+// @ts-expect-error it ain't read-only silly
+Scanner.audioCollection = audioCollectionFixture;
+
+describe("Find API tests", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("findRandom()", () => {
+    it("should return random results", async () => {
+      expect(await findRandom()).toEqual({
+        albums: expect.any(Array),
+        artists: expect.any(Array),
+        audios: expect.any(Array),
+      });
+      expect(getArtistAlbums).toHaveBeenCalled();
+      expect(getAlbumById).toHaveBeenCalled();
+      expect(getAudioById).toHaveBeenCalled();
+    });
+  });
+});
