e359642c98c762a6375574edfdfb9a2a67febc83
Miika Henttonen <miika.henttonen@gmail.com>
2022-07-21 08:25:40 +0300

    Remove urlsafe-base64 dependency

--- src/urlsafe-base64.spec.ts
+++ src/urlsafe-base64.spec.ts
@@ -1,15 +1,18 @@
 import UrlSafeBase64 from "./urlsafe-base64";
 
 describe("UrlSafeBase64", () => {
+  const pangramPlain = "The quick brown fox jumps over the lazy dog";
+  const pangramBase64 = "VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw";
+
   describe("encode", () => {
     it("should encode to base64", () => {
-      expect(UrlSafeBase64.encode("foo")).toBe("Zm9v");
+      expect(UrlSafeBase64.encode(pangramPlain)).toBe(pangramBase64);
     });
   });
 
   describe("decode", () => {
     it("should decode from base64", () => {
-      expect(UrlSafeBase64.decode("Zm9v")).toBe("foo");
+      expect(UrlSafeBase64.decode(pangramBase64)).toBe(pangramPlain);
     });
   });
 });
--- src/urlsafe-base64.ts
+++ src/urlsafe-base64.ts
@@ -1,11 +1,20 @@
-import UrlSafeBase64 from "urlsafe-base64";
-
 const encode = (s: string): string => {
-  return UrlSafeBase64.encode(Buffer.from(s));
+  return Buffer.from(s)
+    .toString("base64")
+    .replace(/\+/g, "-") // Convert '+' to '-'
+    .replace(/\//g, "_") // Convert '/' to '_'
+    .replace(/=+$/, ""); // Remove ending '='
 };
 
 const decode = (s: string): string => {
-  return UrlSafeBase64.decode(s).toString("utf-8");
+  // Append removed '=' chars
+  s += Array(5 - (s.length % 4)).join("=");
+
+  s = s
+    .replace(/-/g, "+") // Convert '-' to '+'
+    .replace(/_/g, "/"); // Convert '_' to '/'
+
+  return Buffer.from(s, "base64").toString("utf8");
 };
 
 export default {
