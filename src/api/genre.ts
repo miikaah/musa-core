74c355737460f628f0747db11279857af6490f5c
Miika Henttonen <miika.henttonen@gmail.com>
2022-08-20 19:27:53 +0300

    Remove console.log

--- src/api/genre.ts
+++ src/api/genre.ts
@@ -1,6 +1,5 @@
 import { getAllGenres as getAllGenresFromDb } from "../db";
 
 export const getAllGenres = async () => {
-  console.log(await getAllGenresFromDb());
   return getAllGenresFromDb();
 };
