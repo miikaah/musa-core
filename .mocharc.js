f35f4ec9fb080985aace8ce26904e456af7a5bb2
Miika Henttonen <miika.henttonen@gmail.com>
2022-09-18 13:47:02 +0300

    Change mocha reporter to list

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,7 @@
 // https://github.com/mochajs/mocha/blob/master/example/config/.mocharc.js
 
 module.exports = {
-  reporter: "min",
+  reporter: "list",
   require: "mochaHooks.ts",
   spec: ["src/**/*.spec.ts"],
 };
