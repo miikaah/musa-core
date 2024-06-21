cba3ee4cc25efcae68bf28b68faf384d7cf1eaed
Miika Henttonen <miika.henttonen@gmail.com>
2024-06-21 15:16:16 +0300

    Fix paths

--- addons/normalization/main.mjs
+++ addons/normalization/main.mjs
@@ -4,11 +4,11 @@ const requireAddon = createRequire(import.meta.url);
 const [, , filepath] = process.argv;
 
 try {
-  const normalization = requireAddon("../../bin/normalization-v1.0.0-darwin-arm64.node");
+  const normalization = requireAddon("../../bin/normalization-v1.0.0-darwin-arm64.node1");
 
   console.log(normalization.calc_loudness(["", filepath]));
 } catch (error) {
-  if (error.message.includes("Cannot find module './build/Release/normalization'")) {
+  if (error.message.includes("Cannot find module '../../bin/normalization")) {
     console.warn("Failed to require normalization addon. Ignoring.");
   } else {
     console.error(error);
