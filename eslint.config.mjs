1544f265747309b64e3a2ebb4953b321ca28323a
Miika Henttonen <miika.henttonen@gmail.com>
2024-08-06 17:59:36 +0300

    Start using shared eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import eslint from "@eslint/js";
+import eslintConfigMusa from "@miikaah/eslint-config-musa";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
@@ -11,43 +11,5 @@ export default tseslint.config(
   eslintConfigPrettier,
   // @ts-expect-error Prettier is behind the times
   eslintPluginPrettierRecommended,
-  {
-    ignores: ["lib/**/*"],
-  },
-  {
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "module",
-      globals: {
-        ...globals.node,
-      },
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    rules: {
-      "prettier/prettier": 1,
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-misused-promises": "off",
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-call": "off",
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-      "@typescript-eslint/prefer-promise-reject-errors": "off",
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/restrict-template-expressions": "off",
-      "no-return-await": "off", // Note: you must disable the base rule as it can report incorrect errors
-      "@typescript-eslint/return-await": "error",
-    },
-  },
-  {
-    files: ["src/**/*.spec.ts"],
-    rules: {
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/ban-ts-comment": "off",
-    },
-  },
+  ...eslintConfigMusa.getConfig(import.meta.dirname),
 );
